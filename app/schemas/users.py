3a710f32b421da58900150f6b835583437730127
alan desktop <alan.alcantargz@gmail.com>
2025-10-14 20:13:05 -0600

    some user schemas were added and a little research about exceptions handler by fastapi was made

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -1,7 +1,16 @@
 from pydantic import BaseModel, EmailStr
+from datetime import datetime
 
 
-class UserSignup(BaseModel):
+class UserBase(BaseModel):
     username: str
     email: EmailStr
+
+class UserSignup(UserBase):
     password: str
+
+class UserPublic(UserBase):
+    uuid: str
+    is_active: bool
+    is_verified: bool
+    created_at: datetime
